1a5e50e5c389e873e3b85f755757660f729504bb
kaique mendonça <Kaique.mendonca855@Gmail.com>
2025-04-02 18:40:28 +0100

    criar pasta automatizado

---
+++ projetospyauto.py
@@ -0,0 +1,6 @@
+import pyautogui
+pyautogui.click(x=1545,y=390, duration=2)
+pyautogui.click(button="right")
+pyautogui.click(x=1566,y=550,duration=2)
+pyautogui.click(x=1396,y=552,duration=2)
+pyautogui.click(button="left")
