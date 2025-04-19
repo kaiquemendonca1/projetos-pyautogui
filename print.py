78a27a1af6490fa3d98f1e96aa0931b26f6502bf
kaique mendonça <Kaique.mendonca855@Gmail.com>
2025-04-19 09:40:34 +0100

    pyautogui print

---
+++ print.py
@@ -0,0 +1,3 @@
+import pyautogui
+
+pyautogui.screenshot("calculadora.jpg",region=(1333,171,400,669))
--- projetospyauto.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-pyautogui.click(x=1469,y=314, duration=2)
-pyautogui.click(button="right")
-pyautogui.click(x=1485,y=510,duration=2)
-pyautogui.click(x=1396,duration=2)
-pyautogui.click()
